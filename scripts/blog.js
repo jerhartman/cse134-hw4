f7b17a7f9217d278b0f28de3acff9552e881236e
jeremy hartman <63078037+jerhartman@users.noreply.github.com>
2023-02-23 10:41:22 -0800

    added promise to checkLocalStorage

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,63 +3,75 @@
 // import dialog module functions
 import { dialogEventInit, editEvent, deleteEvent } from '/scripts/cruddialog.js';
 
+var newsAPIkey = '9dec0d80721a49b282b5b971f8a352ef';
+
 // before rendering the page, check local storage to see if the user
 // has already loaded the page before. if not, we add to local storage
 // the 5 top articles using the news api
 function checkLocalStorage() {
-    let allBlogPosts = JSON.parse(localStorage.getItem('allBlogPosts'));
-    // check if there are blogs in localstorage
-    if(allBlogPosts) {
-        console.log('blogs found');
-        return;
-    }
-    // if not, we add blogs from the news api
-    else {
-        var url = 'https://newsapi.org/v2/top-headlines?country=us&general&q=&pageSize=20&page=1e&apiKey=9dec0d80721a49b282b5b971f8a352ef';
-        var req = new Request(url);
-        fetch(req)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(json) {
-            // take first 5 articles from json data
-            let articles = json.articles.slice(0, 5);
-            // array to add blogs to, then add to localstorage
-            let blogArr = [];
-            // iterate over articles and extract desired data
-            for (var i = 0; i < articles.length; i++) {
-                let blogPost = {
-                    id: self.crypto.randomUUID(),
-                    title: articles[i].title,
-                    date: articles[i].publishedAt,
-                    summary: articles[i].description,
-                    link: articles[i].url
+    return new Promise((resolve, reject) => {
+        let blogsFound = JSON.parse(localStorage.getItem('allBlogPosts'));
+        // check if there are blogs in localstorage
+        if(blogsFound) {
+            console.log('blogs found');
+            resolve(blogsFound);
+        }
+        // if not, we add blogs from the news api
+        else {
+            let url = 'https://newsapi.org/v2/top-headlines?country=us&general&q=&pageSize=20&page=1e&apiKey=' + newsAPIkey;
+            let req = new Request(url);
+            fetch(req)
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                // take first 5 articles from json data
+                let articles = json.articles.slice(0, 5);
+                // array to add blogs to, then add to localstorage
+                let allBlogPosts = [];
+                // iterate over articles and extract desired data
+                for (var i = 0; i < articles.length; i++) {
+                    let blogPost = {
+                        id: self.crypto.randomUUID(),
+                        title: articles[i].title,
+                        date: articles[i].publishedAt,
+                        summary: articles[i].description,
+                        link: articles[i].url
                 };
-                // add blog post object to blogArr
-                blogArr.push(blogPost);
-            }
-            // add blog posts to localstorage
-            localStorage.setItem('allBlogPosts', JSON.stringify(blogArr));
-        })
-        // catch any errors
-        .catch(function(error) {
-        console.log(error);
-        });
-    }
-}
-
+                    // add blog post object to blogArr
+                    allBlogPosts.push(blogPost);
+                }
+                // add blog posts to localstorage
+                localStorage.setItem('allBlogPosts', JSON.stringify(allBlogPosts));
+                resolve(allBlogPosts);
+            })
+            // catch any errors
+            .catch(function(error) {
+                console.log(error);
+                reject(error);
+            });
+        }
+    });
+} 
 
 // run when page loads,
 // populate page with blogs from storage
 function pageInit() {
     // check local storage for blogs
-    checkLocalStorage();
-    // get blog post array from local storage (implement at end)
-    let allBlogPosts = JSON.parse(localStorage.getItem('allBlogPosts'));
-    // render all blogs in local storage
-    renderBlogs(allBlogPosts);
-    // add event listener to Add Blog Post button
-    dialogEventInit();
+    checkLocalStorage()
+    .then(function(allBlogPosts) {
+        // render all blogs in local storage
+        renderBlogs(allBlogPosts);
+        // add event listener to Add Blog Post button
+        dialogEventInit();
+    });
+
+    // // get blog post array from local storage (implement at end)
+    // let allBlogPosts = JSON.parse(localStorage.getItem('allBlogPosts'));
+    // // render all blogs in local storage
+    // renderBlogs(allBlogPosts);
+    // // add event listener to Add Blog Post button
+    // dialogEventInit();
 }
 
 // populate page with blogs
